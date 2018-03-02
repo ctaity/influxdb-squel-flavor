455988318492fab01cfcc011e42aa3bb2f61e5a0
Tajtelbaum Claudio <ctajtelbaum@tvpublica.com.ar>
2018-03-02 16:14:09 -0300

    change to module convert

--- index.js
+++ index.js
@@ -1,16 +1,60 @@
 "use strict"
 
-require("./influx-flavor")
-const squel = require('squel').useFlavour('influx');
+const squel = require('squel');   // to use util.inherits() from node.js
 
-let query = squel.select()
-    .from("year.ds_traffic")
-    .field("sum(bandwith)", "bandwith")
-    .group("time(1d)")
-    .period('365d')
-    .fill(0)
-    .limit(10)
+squel.flavours['influx'] = function (_squel) {
+    let cls = _squel.cls;
 
-console.log(query.toString())
-console.log(query.clone().where("a=1").toString())
-console.log(query.toString())
+    cls.Fill = class extends cls.Block {
+        constructor(options) {
+            super(options);
+            this._command = '';
+        }
+
+        _toParamString() {
+            console.log("_toParamString")
+            return {
+                text: this._command.toUpperCase(),
+                values: [],
+            };
+        }
+
+        fill(argument) {
+            this._command = `fill(${argument})`;
+        };
+
+    }
+
+    cls.InfluxWhere = class extends cls.WhereBlock {
+
+        period(period){
+            this.where(`time > now() - ${period}`)
+        }
+
+    }
+
+    cls.Select = class extends cls.QueryBuilder {
+        constructor(options, blocks = null) {
+            blocks = blocks || [
+                new cls.StringBlock(options, 'SELECT'),
+                new cls.FunctionBlock(options),
+                // new cls.DistinctBlock(options),
+                new cls.GetFieldBlock(options),
+                new cls.FromTableBlock(options),
+                // new cls.JoinBlock(options),
+                new cls.InfluxWhere(options),
+                // new cls.Period(options),
+                new cls.GroupByBlock(options),
+                // new cls.HavingBlock(options),
+                new cls.OrderByBlock(options),
+                // new cls.OffsetBlock(options),
+                // new cls.UnionBlock(options),
+                new cls.Fill(options),
+                new cls.LimitBlock(options)
+            ];
+
+            super(options, blocks);
+        }
+    }
+
+};
